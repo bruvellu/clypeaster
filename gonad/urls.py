96e6f20d422ddcc6f0c84f5374931a3be2d57342
Bruno Vellutini <organelas@gmail.com>
2011-10-11 21:25:21 -0300

    Transfer app urls to app folder

---
+++ gonad/urls.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls.defaults import *
+from gonad.views import *
+
+urlpatterns = patterns('',
+        # Home.
+        (r'^$', main_page),
+        # Where you stage gonads.
+        (r'^staging/$', staging_page),
+        # List of unstaged sections.
+        (r'^unstaged/$', unstaged_page),
+        # List of pre-staged sections.
+        (r'^prestaged/$', prestaged_page),
+        # List of staged sections.
+        (r'^staged/$', staged_page),
+
+        # Tubule page.
+        url(r'^tubule/(\d+)/$', tubule_page, name='tubule_url'),
+        # Section page.
+        url(r'^section/(\d+)/$', section_page, name='section_url'),
+)
--- urls.py
+++ urls.py
@@ -2,7 +2,6 @@
 
 import os
 from django.conf.urls.defaults import *
-from gonad.views import *
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,22 +10,8 @@ admin.autodiscover()
 static = os.path.join(os.path.dirname(__file__), 'static')
 
 urlpatterns = patterns('',
-        # Home.
-        (r'^$', main_page),
-        # Where you stage gonads.
-        (r'^staging/$', staging_page),
-        # List of unstaged sections.
-        (r'^unstaged/$', unstaged_page),
-        # List of pre-staged sections.
-        (r'^prestaged/$', prestaged_page),
-        # List of staged sections.
-        (r'^staged/$', staged_page),
-
-        # Tubule page.
-        url(r'^tubule/(\d+)/$', tubule_page, name='tubule_url'),
-        # Section page.
-        url(r'^section/(\d+)/$', section_page, name='section_url'),
-
+        # Gonads.
+        url(r'^', include('analysis.gonad.urls')),
         # Admin.
         (r'^admin/', include(admin.site.urls)),
         # Site media.
