6260dc08ea33c8f47c6c9ea74fabd07ef9700a14
Bruno Vellutini <organelas@gmail.com>
2012-05-12 20:41:04 +0200

    Make common data frame for object and adjustments

--- gonad/stats.py
+++ gonad/stats.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 from datetime import date
 from numpy import array, mean, std
 from os import remove
@@ -9,16 +10,18 @@ import matplotlib.pyplot as plt
 
 from settings import MEDIA_ROOT
 
-#TODO Mean of tubule measurements by date (all tubules, not by specimen).
-# Cross section area, germ layer area, and GLA index.
-
 
 class TubulePlots:
     def __init__(self, tubules):
+        # Create instance for queryset.
         self.tubules = tubules
 
-        # Build data frame.
-        self.data = self.build_data_frame(tubules)
+        # Build data frame and instantiate "self.data".
+        self.build_data_frame(self.tubules)
+
+        # Define grouped data by date.
+        self.avg_by_date = self.data.groupby('dates').mean()
+        self.std_by_date = self.data.groupby('dates').std()
 
         # Build plots.
         self.scatter_gla_by_cross()
@@ -26,7 +29,7 @@ class TubulePlots:
         self.plot_gla_by_date()
 
     def build_data_frame(self, tubules):
-        '''Build dataframes for tubules plots using pandas.'''
+        '''Build data frames for tubules plots using pandas.'''
         # Get objects.
         ids = tubules.values_list('id', flat=True)
         cross_sections = tubules.values_list('cross_section', flat=True)
@@ -42,13 +45,16 @@ class TubulePlots:
                 'dates': dates,
                 }
 
-        # Create DataFrame for tubule data.
-        data_frame = DataFrame(data_dic, index=ids)
+        #TODO Write csv with data frame for history.
 
-        return data_frame
+        # Create DataFrame for tubule data.
+        self.data = DataFrame(data_dic, index=ids)
 
     def scatter_gla_by_cross(self):
         '''Scatter plot with GLA by cross section area.'''
+        # Define plot name.
+        plot_name = 'gla_by_cross'
+
         # Clear plot.
         plt.clf()
 
@@ -57,8 +63,9 @@ class TubulePlots:
         gla_indexes = self.data['gla_index'].tolist()
 
         # Define paths.
-        png_path = join(MEDIA_ROOT, 'plots/gla_by_cross.png')
-        pdf_path = join(MEDIA_ROOT, 'plots/gla_by_cross.pdf')
+        png_path = join(MEDIA_ROOT, 'plots/%s.png' % plot_name)
+        pdf_path = join(MEDIA_ROOT, 'plots/%s.pdf' % plot_name)
+        csv_path = join(MEDIA_ROOT, 'plots/%s.csv' % plot_name)
 
         # Define figure to handle the limits better.
         figure = plt.figure()
@@ -75,23 +82,24 @@ class TubulePlots:
 
     def plot_areamean_by_date(self):
         '''Build plot for tubules measurements grouped by date.'''
+
+        # Define plot name.
+        plot_name = 'areamean_by_date'
+
         # Clear plot.
         plt.clf()
 
-        # Define grouped data.
-        avg_data = self.data.groupby('dates').mean()
-        std_data = self.data.groupby('dates').std()
-
         # Define paths.
-        png_path = join(MEDIA_ROOT, 'plots/areamean_by_date.png')
-        pdf_path = join(MEDIA_ROOT, 'plots/areamean_by_date.pdf')
+        png_path = join(MEDIA_ROOT, 'plots/%s.png' % plot_name)
+        pdf_path = join(MEDIA_ROOT, 'plots/%s.pdf' % plot_name)
+        csv_path = join(MEDIA_ROOT, 'plots/%s.csv' % plot_name)
 
         # Data points.
-        x = avg_data.index.tolist()
-        germ_layer = avg_data['germ_layer'].tolist()
-        cross_section = avg_data['cross_section'].tolist()
-        germ_layer_std = std_data['germ_layer'].tolist()
-        cross_section_std = std_data['cross_section'].tolist()
+        x = self.avg_by_date.index.tolist()
+        germ_layer = self.avg_by_date['germ_layer'].tolist()
+        cross_section = self.avg_by_date['cross_section'].tolist()
+        germ_layer_std = self.std_by_date['germ_layer'].tolist()
+        cross_section_std = self.std_by_date['cross_section'].tolist()
 
         # Define figure to handle the limits better.
         figure = plt.figure()
@@ -115,23 +123,28 @@ class TubulePlots:
         figure.savefig(png_path)
         figure.savefig(pdf_path)
 
+        #TODO Write CSV.
+
+        #TODO Return data frame.
+
     def plot_gla_by_date(self):
         '''Build plot for gla indexes grouped by date.'''
+
+        # Define plot name.
+        plot_name = 'gla_by_date'
+
         # Clear plot.
         plt.clf()
 
-        # Define grouped data.
-        avg_data = self.data.groupby('dates').mean()
-        std_data = self.data.groupby('dates').std()
-
         # Define paths.
-        png_path = join(MEDIA_ROOT, 'plots/gla_by_date.png')
-        pdf_path = join(MEDIA_ROOT, 'plots/gla_by_date.pdf')
+        png_path = join(MEDIA_ROOT, 'plots/%s.png' % plot_name)
+        pdf_path = join(MEDIA_ROOT, 'plots/%s.pdf' % plot_name)
+        csv_path = join(MEDIA_ROOT, 'plots/%s.csv' % plot_name)
 
         # Data points.
-        x = avg_data.index.tolist()
-        gla_index = avg_data['gla_index'].tolist()
-        gla_std = std_data['gla_index'].tolist()
+        x = self.avg_by_date.index.tolist()
+        gla_avg = self.avg_by_date['gla_index'].tolist()
+        gla_std = self.std_by_date['gla_index'].tolist()
 
         # Define figure to handle the limits better.
         figure = plt.figure()
@@ -142,7 +155,7 @@ class TubulePlots:
         plt.title(u'GLA index grouped by date')
 
         # Plot data.
-        plt.errorbar(x, gla_index, yerr=gla_std, fmt='-', label=u'GLA index', color='blue')
+        plt.errorbar(x, gla_avg, yerr=gla_std, fmt='-', label=u'GLA index', color='blue')
         plt.xlim(xmin=date(2006, 12, 01), xmax=date(2007, 12, 01))
         plt.ylim(ymin=0.0, ymax=1.0)
         figure.autofmt_xdate()
@@ -151,6 +164,9 @@ class TubulePlots:
         figure.savefig(png_path)
         figure.savefig(pdf_path)
 
+        #TODO Write CSV.
+
+        #TODO Return data frame.
 
 #TODO Correlation between GLA and tubule area. Does GLA vary with tubule size? 
 # If yes, how to normalize the values?
